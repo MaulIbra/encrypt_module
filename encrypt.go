bd97cbd1ef8d7fd5575dae2fa889aaacf0fb9a16
maulana <maulana@mymindstores.com>
2020-11-24 10:16:11 +0700

    1st version encrypt

---
+++ encrypt.go
@@ -0,0 +1,23 @@
+package encrypt
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"log"
+)
+
+func Encrypt(pwd []byte) string{
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(hash)
+}
+
+func CompareEncrypt(hashedPwd string, plainPwd []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+	if err != nil {
+		return false
+	}
+	return true
+}
